ee50bcd9411c6780708c34e2712b831852ce3e4d
Naoya Otsuka <otknoy@gmail.com>
2019-02-17 03:21:52 +0900

    social link

--- src/components/profile.js
+++ src/components/profile.js
@@ -24,17 +24,17 @@ class MyProfile extends HTMLElement {
   <ul>
     <li>${profile.name}</li>
     <li>${profile.mail}</li>
-    <li><a href="https://github.com/${profile.github}">GitHub</a></li>
-    <li>
-      <a href="https://twitter.com/${profile.twitter}">
-       <img
-         src="./img/Twitter_Social_Icon_Rounded_Square_Color.svg"
-         width="16px"
-       >
-      </a>
-    </li>
-    <li><a href="https://www.facebook.com/${profile.facebook}">Facebook</a></li>
   </ul>
+
+  <div>
+    <a href="https://github.com/${profile.github}">GitHub</a>
+    <a href="https://twitter.com/${profile.twitter}">
+     <img
+       src="./img/Twitter_Social_Icon_Rounded_Square_Color.svg"
+       width="16px">
+    </a>
+    <a href="https://www.facebook.com/${profile.facebook}">Facebook</a>
+  </div>
 
   <my-skill-and-interest></my-skill-and-interest>
 </section>
