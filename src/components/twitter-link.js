de98960ad4ad548358ad02920f2eee0045d84def
Naoya Otsuka <otknoy@gmail.com>
2019-02-18 00:18:01 +0900

    attribute observation sample

--- src/components/twitter-link.js
+++ src/components/twitter-link.js
@@ -5,13 +5,25 @@ class TwitterLink extends HTMLElement {
     const account = this.getAttribute('account');
 
     this.attachShadow({mode: 'open'});
-    this.shadowRoot.innerHTML = `
+    this.shadowRoot.innerHTML = this.render(account);
+  }
+
+  render(account) {
+    return `
 <a href="https://twitter.com/${account}">
   <img
    src="./img/Twitter_Social_Icon_Rounded_Square_Color.svg"
    width="16px">
 </a>
 `;
+  }
+
+  static get observedAttributes() { return ['account']; }
+
+  attributeChangedCallback(attr, oldValue, newValue) {
+    if (attr == 'account') {
+      this.shadowRoot.innerHTML = this.render(newValue);
+    }
   }
 }
 
