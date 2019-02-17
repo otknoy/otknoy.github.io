0a171806312208ea8dc651db51f581459a62be97
Naoya Otsuka <otknoy@gmail.com>
2019-02-18 00:31:24 +0900

    profile style

--- src/components/profile.js
+++ src/components/profile.js
@@ -19,20 +19,43 @@ class MyProfile extends HTMLElement {
 
   template(profile) {
     return `
+<style>
+section {
+  padding: 12px;
+}
+
+.profile {
+  display: flex;
+  align-items: center;
+  // justify-content: center;
+}
+
+img {
+  width: 64px;
+  height: 64px;
+}
+
+h1 {
+  margin: 0px;
+}
+</style>
 <section>
   <h1>Profile</h1>
 
-  <img src="${profile.icon}" width="64px">
+  <div class="profile">
+    <img src="${profile.icon}">
 
-  <ul>
-    <li>${profile.name}</li>
-    <li>${profile.mail}</li>
-  </ul>
-
-  <div>
-    <a href="https://github.com/${profile.github}">GitHub</a>
-    <twitter-link account="otknoy"></twitter-link>
-    <a href="https://www.facebook.com/${profile.facebook}">Facebook</a>
+    <ul>
+      <li>${profile.name}</li>
+      <li>${profile.mail}</li>
+      <li>
+        <div>
+          <a href="https://github.com/${profile.github}">GitHub</a>
+          <twitter-link account="otknoy"></twitter-link>
+          <a href="https://www.facebook.com/${profile.facebook}">Facebook</a>
+        </div>
+      </li>
+    </ul>
   </div>
 
   <my-skill-and-interest></my-skill-and-interest>
