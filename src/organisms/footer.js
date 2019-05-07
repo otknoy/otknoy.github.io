7d9c2d8dfa34bbb8eb32d5a5a770bddd49fe25ae
Naoya Otsuka <otknoy@gmail.com>
2019-05-07 17:44:44 +0900

    change footer text to title

--- src/organisms/footer.js
+++ src/organisms/footer.js
@@ -11,18 +11,14 @@ class Footer extends HTMLElement {
   html() {
     return `
     <style>
-      div {
-        background-color: black;
+      :host {
         text-align: center;
-        padding: 4px;
       }
     </style>
     <footer class="footer">
-      <div>
-        <x-text size="small" color="white">
-          (C) 1988-2019 Naoya Otsuka
-        </x-text>
-      </div>
+      <x-title size="small" color="black">
+        (C) 1988-2019 Naoya Otsuka
+      </x-title>
     </footer>
 `;
   }
