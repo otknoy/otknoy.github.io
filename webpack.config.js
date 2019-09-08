fe552499f524f89342dc7f26719aaab5e2b19bd9
Naoya Otsuka <otknoy@gmail.com>
2019-09-08 18:11:59 +0900

    use babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,14 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+	test: /\.js$/,
+	exclude: /node_modules/,
+	loader: 'babel-loader'
+      }
+    ]
   }
 }
