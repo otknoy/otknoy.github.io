008f67316b9bc5f208b271c79847555eca31c2b2
Naoya Otsuka <otknoy@gmail.com>
2020-03-30 15:14:19 +0900

    remove unnecessary component

--- src/Page.js
+++ src/Page.js
@@ -1,24 +1,18 @@
 import React from 'react'
-import styled from 'styled-components'
 
 import Header from './Header'
 import Tile from './Tile'
 import Footer from './Footer'
 
-const Container = styled.div`
-display: flex;
-flex-direction: column;
-`
-
 const Page = () => (
     <>
       <Header />
     
-      <Container>
+      <div>
         <Tile link="https://twitter.com/otknoy">twitter</Tile>
         <Tile link="https://github.com/otknoy/">github</Tile>
         <Tile link="https://otknoy.hatenablog.com/">blog</Tile>
-      </Container>
+      </div>
 
       <Footer />
     </>
