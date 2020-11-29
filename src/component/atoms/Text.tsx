ceb586acda4ce01b8d661275484dee52deeaacd6
Naoya Otsuka <otknoy@gmail.com>
2020-11-29 11:55:34 +0900

    atoms/Text.tsx

---
+++ src/component/atoms/Text.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+const Text: React.FC<{}> = ({ children }) => (
+  <span style={{ fontSize: '16px' }}>{children}</span>
+)
+
+type Props = {
+  href: string
+}
+
+const LinkText: React.FC<Props> = ({ href, children }) => (
+  <a href={href}>
+    <Text>{children}</Text>
+  </a>
+)
+
+export { Text, LinkText }
