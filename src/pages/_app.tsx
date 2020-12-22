3895d3226a25b80a93dea93673c1451019eeebb8
Naoya Otsuka <otknoy@gmail.com>
2020-12-22 22:26:17 +0900

    Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react'
 
+import { useEffect } from 'react'
 import { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+
+import * as gtag from '../lib/gtag'
 
 import 'normalize.css'
 import './style.css'
 
-const App: React.SFC<AppProps> = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-)
+const App: React.SFC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => gtag.pageview(url)
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => router.events.off('routeChangeComplete', handleRouteChange)
+  }, [router.events])
+
+  return <Component {...pageProps} />
+}
 
 export default App
