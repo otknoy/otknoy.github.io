d19ced41b75d86a6cc3013d723a349746aaf500a
Naoya Otsuka <otknoy@gmail.com>
2020-12-28 11:30:43 +0900

    remove header image

--- src/component/organisms/Header.tsx
+++ src/component/organisms/Header.tsx
@@ -5,13 +5,6 @@ import { PageTitle } from '../atoms/Title'
 const Header: React.SFC = () => (
   <header style={{ textAlign: 'center' }}>
     <PageTitle>Naoya Otsuka</PageTitle>
-
-    <img
-      src="https://pbs.twimg.com/profile_banners/43682408/1417191879/1500x500"
-      width="100%"
-      height="160px"
-      loading="lazy"
-    />
   </header>
 )
 
