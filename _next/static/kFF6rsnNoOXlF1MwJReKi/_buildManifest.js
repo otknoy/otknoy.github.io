f2d357100bd6bcced3f1aff65192ffdb2104ebdf
otknoy <otknoy@users.noreply.github.com>
2020-12-28 02:44:06 +0000

    deploy: 5e3680243e1a4fdbcb03e13ce674b13243497f53

--- _next/static/kFF6rsnNoOXlF1MwJReKi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-e249a32711db5de6c6f4.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-85785763783c1e33a9f3.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
