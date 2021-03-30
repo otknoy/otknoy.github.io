7d67ccf7db601bc663e54c6528de7850cd0cd955
Naoya Otsuka <otknoy@gmail.com>
2021-03-30 21:08:58 +0900

    remove unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import Head from 'next/head'
 
 import Template from '../components/templates'
 import Profile from '../components/organisms/Profile'
-import Portfolio from '../components/organisms/Portfolio'
 import Interest from '../components/organisms/Interest'
 
 const Home: React.SFC = () => (
