5ea4d1a065efafa8edec496b2863cdbb9bf303b5
Naoya Otsuka <otknoy@gmail.com>
2021-04-04 11:17:34 +0900

    remove templates.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,26 @@ import * as React from 'react'
 
 import Head from 'next/head'
 
-import Template from '../components/templates'
+import Header from '../components/organisms/Header'
+import Footer from '../components/organisms/Footer'
 import Profile from '../components/organisms/Profile'
 import Interest from '../components/organisms/Interest'
 
 const Home: React.SFC = () => (
-  <Template>
+  <div style={{maxWidth: '410px', margin: '0 auto', padding: '0 8px'}}>
     <Head>
       <title>Naoya Otsuka</title>
       <meta name="description" content="Naoya Otsuka, web engineer" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
     </Head>
 
+    <Header />
+
     <Profile />
     <Interest />
-  </Template>
+
+    <Footer />
+  </div>
 )
 
 export default Home
