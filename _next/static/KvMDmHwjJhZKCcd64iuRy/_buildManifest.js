9e929d156b31898a36fe24462d420e06198466bf
otknoy <otknoy@users.noreply.github.com>
2021-06-18 14:29:50 +0000

    deploy: 6b91736a6cd8a26a05ffad84c2fd27c1168c44f8

--- _next/static/KvMDmHwjJhZKCcd64iuRy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-35d5d815d5fc5c25da3d.js"],"/_error":["static/chunks/pages/_error-8842bdd603648886795b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
