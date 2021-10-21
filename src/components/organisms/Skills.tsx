3b7677a6880755ceee00d9be29b696228a3ff1a1
Naoya Otsuka <otknoy@gmail.com>
2021-10-21 21:08:32 +0900

    jest 27.3.1

--- src/components/organisms/Skills.tsx
+++ src/components/organisms/Skills.tsx
@@ -3,28 +3,30 @@ import React from 'react'
 import { SectionTitle } from '../atoms/Title'
 import { Text } from '../atoms/Text'
 
-const skills: string[] = [
-  'Java, Kotlin, SpringBoot',
-  'Go, Python',
-  'Docker, Kubernetes, Microservices',
-  'Solr',
-  'Information Retrieval',
-  'Human Computer Interaction',
-  'Exploratory Search',
-]
+const Skills: React.FC = () => {
+  const skills: string[] = [
+    'Java, Kotlin, SpringBoot',
+    'Go, Python',
+    'Docker, Kubernetes, Microservices',
+    'Solr',
+    'Information Retrieval',
+    'Human Computer Interaction',
+    'Exploratory Search',
+  ]
 
-const Skills: React.FC = () => (
-  <section>
-    <SectionTitle>Skills</SectionTitle>
+  return (
+    <section>
+      <SectionTitle>Skills</SectionTitle>
 
-    <ul>
+      <ul>
       {skills.map((e, i) => (
-        <li key={i}>
+	<li key={i}>
           <Text>{e}</Text>
         </li>
       ))}
-    </ul>
-  </section>
-)
+      </ul>
+    </section>
+  )
+}
 
 export default Skills
