ddc7291fb674ba1389dd3c4e715738a1b8f590dd
otknoy <otknoy@users.noreply.github.com>
2021-11-05 07:12:39 +0000

    deploy: 3410d09cbb57c6d510d1c042399af1c8013263b5

--- _next/static/LJNeVdHH8PCLFIvDdPQZj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-89fc1acb8f5b9c2d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/portfolio":["static/chunks/pages/portfolio-3472098d7c91a7dd.js"],sortedPages:["/","/_app","/_error","/portfolio"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
