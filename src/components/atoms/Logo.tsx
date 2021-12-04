4133ec10828a34cd84f252ae9c7d44e12037a91f
Naoya Otsuka <otknoy@gmail.com>
2021-12-04 12:41:43 +0900

    simplify

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 
-const Logo: React.FC = ({ children }) => {
-  const style = {
-    fontSize: '24px'
-  }
-  return <h1 style={style}>{children}</h1>
-}
+const Logo: React.FC = ({ children }) => (
+  <h1 style={{ fontSize: '24px' }}>{children}</h1>
+)
 
 export { Logo }
