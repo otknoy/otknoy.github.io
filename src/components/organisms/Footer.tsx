a24bc2c3c5fc3aa0ddf90845628dcc17212111e5
Naoya Otsuka <otknoy@gmail.com>
2021-12-30 16:06:09 +0900

    custom hook

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -2,12 +2,18 @@ import React from 'react'
 
 import { Text } from '../atoms/Text'
 
-const Footer = () => (
-  <footer style={{ textAlign: 'center' }}>
-    <h1 style={{ margin: '0px', padding: '4px' }}>
-      <Text>(C) 1988-{new Date().getFullYear()} Naoya Otsuka</Text>
-    </h1>
-  </footer>
-)
+import { useDateNow } from '../../hooks/date'
+
+const Footer = () => {
+  const now = useDateNow()
+
+  return (
+    <footer style={{ textAlign: 'center' }}>
+      <h1 style={{ margin: '0px', padding: '4px' }}>
+        <Text>(C) 1988-{now.getFullYear()} Naoya Otsuka</Text>
+      </h1>
+    </footer>
+  )
+}
 
 export default Footer
---
+++ src/hooks/date.ts
@@ -0,0 +1,5 @@
+const useDateNow = (): Date => {
+  return new Date()
+}
+
+export { useDateNow }
