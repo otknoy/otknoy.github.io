a7904c638c60169fdc0b7934a63001155af22411
Naoya Otsuka <otknoy@gmail.com>
2021-12-31 14:27:43 +0900

    date test

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
   return (
     <footer style={{ textAlign: 'center' }}>
       <h1 style={{ margin: '0px', padding: '4px' }}>
-        <Text>(C) 1988-{now.getFullYear()} Naoya Otsuka</Text>
+        <Text>{`(C) 1988-${now.getFullYear()} Naoya Otsuka`}</Text>
       </h1>
     </footer>
   )
