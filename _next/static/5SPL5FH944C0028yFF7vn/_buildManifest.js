fcf6e1b3f1aa5cf282e5a90ca6c23c07270d14ee
otknoy <otknoy@users.noreply.github.com>
2022-05-22 07:09:54 +0000

    deploy: d0d547e2c0578d4fc13d3a6b66dca407702eca6a

---
+++ _next/static/5SPL5FH944C0028yFF7vn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8c313c7e5c8bd148.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/portfolio":["static/chunks/pages/portfolio-881b3080547a7b73.js"],sortedPages:["/","/_app","/_error","/portfolio"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
