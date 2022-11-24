9b93d98d2614ae9e51513ceca243b5c1ec9a1720
otknoy <otknoy@users.noreply.github.com>
2022-11-24 15:28:40 +0000

    deploy: ed75014f2ff94eaa0bb46200344f371b0a1000d2

--- _next/static/BeOxNavMeLHtjUigTQUof/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c565dbc0c49483c1.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],"/portfolio":["static/chunks/pages/portfolio-217323322bfdf86b.js"],sortedPages:["/","/_app","/_error","/portfolio"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
