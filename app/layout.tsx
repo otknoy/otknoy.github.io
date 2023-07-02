08d8f66f9774132b7a80b68d0c32924f79fc5ca7
Naoya Otsuka <otknoy@gmail.com>
2023-07-02 18:10:25 +0900

    add gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import 'normalize.css'
 
+import { GA_TRACKING_ID } from '../lib/gtag'
+
 export const metadata = {
   title: 'Naoya Otsuka',
   description: "Naoya Otsuka's profile",
   viewport: {
     width: 'device-width',
-    initialScale: 1,
-  },
+    initialScale: 1
+  }
 }
 
 export default function RootLayout({
@@ -16,6 +18,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script>
+          dangerouslySetInnerHTML=
+          {{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+          }}
+        </script>
+      </head>
       <body>{children}</body>
     </html>
   )
