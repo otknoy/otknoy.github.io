2acbfb4118cb8a66ae23a3a5ff94da2636dee7e8
otknoy <otknoy@users.noreply.github.com>
2023-07-02 09:13:51 +0000

    deploy: b69fda39ad34661439171d5ff26061306d08a788

--- _next/static/XOFWSsWRuzk5C2BeQhB_E/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4d9a815744b30c3c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],"/portfolio":["static/chunks/pages/portfolio-b1822fb00d5e8d5e.js"],sortedPages:["/","/_app","/_error","/portfolio"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
